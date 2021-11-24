4986e637a8843a0efe5f5e17bcc4897926e988b8
sugar2507 <72238592+sugar2507@users.noreply.github.com>
2021-11-24 16:30:59 +0700

    All Product 24-11 4:25

---
+++ THDShop/THDShop/Controllers/HomeUserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using THDShop.ViewModel.Product;
+
+namespace THDShop.Controllers
+{
+    public class HomeUserController : Controller
+    {
+        // GET: HomeUser
+        QLLaptopShopEntities _db = new QLLaptopShopEntities();
+
+        public HomeUserController()
+        {
+            ProductSingleton.Instance.Init(_db);
+        }
+
+        public ActionResult Index()
+        {
+            var query = _db.PRODUCTS.OrderByDescending(x => x.NAME);
+            return View(query);
+
+        }   
+    }
+}
---
+++ THDShop/THDShop/ViewModel/Product/CreateProductInput.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace THDShop.ViewModel.Product
+{
+    public class CreateProductInput
+    {
+        public CreateProductInput()
+        {
+            IMAGE = "~/Assets/Images/upload.png";
+        }
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Bạn chưa nhập tên ")]
+        [Display(Name = "Tên sản phẩm")]
+        public string NAME { get; set; }
+
+        [Required(ErrorMessage = "Bạn chưa chọn loại sản phẩm")]
+        [Display(Name = "Loại sản phẩm")]
+        public int? IDCATEGORY { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Số lượng không được âm")]
+        [Required(ErrorMessage = "Bạn chưa nhập số lượng")]
+        [Display(Name = "Số lượng")]
+        public int? QUANTITY { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Giá không được âm")]
+        [Required(ErrorMessage = "Bạn chưa nhập giá bán")]
+        [Display(Name = "Giá Bán")]
+        public double? PRICE { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Giá không được âm")]
+        [Required(ErrorMessage = "Bạn chưa nhập giá gốc")]
+        [Display(Name = "Giá Gốc")]
+        public double? ORI_PRICE { get; set; }
+
+
+        [Required(ErrorMessage = "Bạn chưa nhập mô tả")]
+        [Display(Name = "Mô tả")]
+        public string DESCRIPTION { get; set; }
+        [Required(ErrorMessage = "Bạn chưa nhập mô tả CPU")]
+        [Display(Name = "Mô tả CPU")]
+        public string DESCRIPTION_CPU { get; set; }
+        [Required(ErrorMessage = "Bạn chưa nhập mô tả Ram")]
+        [Display(Name = "Mô tả CPU")]
+        public string DESCRIPTION_RAM { get; set; }
+        [Required(ErrorMessage = "Bạn chưa nhập mô tả dung lượng")]
+        [Display(Name = "Mô tả Dung lượng")]
+        public string DESCRIPTION_STORAGE { get; set; }
+        [Required(ErrorMessage = "Bạn chưa nhập mô tả Card")]
+        [Display(Name = "Mô tả Card")]
+        public string DESCRIPTION_CARD { get; set; }
+        [Required(ErrorMessage = "Bạn chưa nhập mô tả khung hình")]
+        [Display(Name = "Mô tả Khung hình")]
+        public string DESCRIPTION_SCREEN { get; set; }
+        [Required(ErrorMessage = "Bạn chưa nhập mô tả cân nặng")]
+        [Display(Name = "Mô tả Cân nặng")]
+        public string DESCRIPTION_WEIGHT { get; set; }
+
+        public string IMAGE { get; set; }
+
+
+        [NotMapped]
+        public HttpPostedFileBase UploadImage { get; set; }
+    }
+    public class UpdateProductInput : CreateProductInput
+    {
+
+    }
+}
